a58321d55f7f87ccfc5046a77bf2dfda47302e3c
Javier <javier.sistemas.arg@gmail.com>
2018-05-02 05:42:47 -0300

    Arguments and backups rotation
    Script can now be called with arguments
    
    	-d daily (default)
    	-w weekly
    	-m monthly
    
    They will cause the script to save the files in different folders
    Also determine what files should be deleted (rotation)

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -4,10 +4,10 @@ db_host="localhost"
 db_user="root"
 db_pass="yourpassword"
 
-date=$(date +"%d-%b-%Y")
+datetime=$(date +"%d-%b-%Y-%H-%M-%S")
 
 mysql_path="/var/lib/mysql/"
-backups_path="/backups/mysql"
+backups_path="/backups/mysql/daily"
 
 dbs=( $(find $mysql_path -maxdepth 1 -type d -printf '%P\n') )
 
@@ -20,5 +20,50 @@ do
 		continue
 	fi
 
-	mysqldump --user=$db_user --password=$db_pass --host=$db_host $db | gzip -c > $backups_path/$db-$date.gz
+	while getopts ":dwm" opt; do
+  		case $opt in
+    			d)
+				# Daily
+				# Path already set as default in case script is called without arguments
+				backup_type="daily"
+      				;;
+			w)
+				# Weekly
+				backup_type="weekly"
+				backups_path="/backups/mysql/weekly"
+				;;
+                        m)
+                                # Monthly
+				backup_type="monthly"
+                                backups_path="/backups/mysql/monthly"
+                                ;;
+    			\?)
+      				echo "Invalid option: -$OPTARG" >&2
+      				;;
+  		esac
+	done
+
+
+        if [ ! -d $backups_path ]; then
+	        mkdir -p $backups_path;
+        fi
+
+	mysqldump --user=$db_user --password=$db_pass --host=$db_host $db | gzip -c > $backups_path/$db-$datetime.gz
+
+	# Backups rotation, we'll preserve:
+	#
+	# Monthly backups from the last 365 days 
+	# Weekly backups from the last 30 days
+	# Daily backups from the last 7 days
+	#
+	# And delete anything older 
+	if [ "backup_type" == "monthly" ]; then
+  		time=365
+	elif [ "backup_type" == "weekly" ]; then
+		time=30
+        else
+		time=7
+        fi
+
+	find $backups_path/ -mtime +${time} -exec rm {} \;
 done
